ef30a396f7ff0417d44dda2bdb9fea71f7499863
Liam Brenner <liam.brenner@gmail.com>
2016-07-25 15:04:41 +1000

    Release 0.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='wagtailvideos',
-    version='0.1.0',
+    version='0.1.1',
     description="A wagtail module for uploading and displaying videos in various codecs.",
     long_description=readme,
     author='Takeflight',
